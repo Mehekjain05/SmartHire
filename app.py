4caa2e25f56e4da25966457bdb171f1f3277357c
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2023-08-03 20:19:20 +0530

    second commit

--- app.py
+++ app.py
@@ -9,11 +9,33 @@ def hello_world():
 def home():
     return render_template('index.html')
 
+@app.route('/category')
+def category():
+    return render_template('category.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/job-detail')
+def job_detail():
+    return render_template('job-detail.html')
+
+@app.route('/job-list')
+def job_list():
+    return render_template('job-list.html')
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html')
+
+@app.route('/404')
+def error_404():
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
